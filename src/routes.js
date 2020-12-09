3a63bc4764ac898bb0827b95a1773411e5374033
Mars_Clarke <marsthecoder@outlook.com>
2020-12-09 14:11:20 -0700

    step 2 done?

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Home from "./src/components/Home/Home";
-import About from "./src/components/About/About";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 export default (
   <Switch>
